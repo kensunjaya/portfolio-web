067421a430517502cb71d16c023b4995633fda22
kensunjaya <kennethsunjaya@gmail.com>
2025-12-31 15:47:09 +0700

    fix: update profile picture source to use new image

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -104,7 +104,7 @@ export const ContactSection = () => {
               className="mb-10"
             >
               <Image 
-                src="/profile_enhanced.webp"
+                src="/profile_pic.webp"
                 alt="Kenneth Sunjaya"
                 draggable={false}
                 width={breakpoint === 'xs' || breakpoint === "sm" ? 300 : breakpoint === "md" ? 350 : breakpoint === 'lg' ? 400 : 500}
